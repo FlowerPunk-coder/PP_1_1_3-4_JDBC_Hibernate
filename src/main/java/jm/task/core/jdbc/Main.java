b0d43fdffc36de68d365808f23a3a5c307f23aa0
Max <maxim.ketikov@gmail.com>
2022-08-03 21:13:47 +0300

    1.1.3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Максим", "Кетиков", (byte) 41);
+        userDaoJDBC.saveUser("Иван", "Минаев", (byte) 24);
+        userDaoJDBC.saveUser("Федор", "Вознесенский", (byte) 35);
+        userDaoJDBC.saveUser("Лев", "Токарев", (byte) 16);
+        List<User> users = userDaoJDBC.getAllUsers();
+        for(User user : users) {
+            System.out.println(user.toString());
+        }
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,15 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String DB = "jdbc:mysql://localhost:3312/pp";
+    private final static String LOGIN = "user";
+    private final static String PASS = "password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB, LOGIN, PASS);
+    }
 }
